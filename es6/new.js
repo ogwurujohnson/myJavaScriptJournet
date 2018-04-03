13c6146bac0868df6d63340047ed5d0fcc210867
blackhatjohnny <ogwurujohnson@gmail.com>
2018-04-03 12:04:23 +0100

    es6-let

--- es6/new.js
+++ es6/new.js
@@ -1,2 +1,25 @@
 "use strict";
+let movie = 'Good Will Hunting';
 
+function theNotebook(){
+    let movie = 'The Notebook';
+    return movie;
+}
+console.log(theNotebook());
+console.log(movie); 
+
+function buckyFunction(){
+    let isHorse = true;
+    let saying = 'Bacon is good';
+    console.log('\nBefore if:', saying);
+
+    if(isHorse){
+        let saying = 'I am a horse';
+        console.log('Inside if:', saying);
+    }
+
+    console.log('After if:', saying);
+
+}
+
+buckyFunction();
