a9bdd1060cbcd3a6f07436b84185dc594450978b
blackhatjohnny <ogwurujohnson@gmail.com>
2018-04-03 13:28:39 +0100

    es6-inheritance

--- es6/inheritance.js
+++ es6/inheritance.js
@@ -8,12 +8,39 @@ class Person{
         this.weight = weight;
     }
 
-    //a method
+    //a method in the classs
     displayWeight(){
         console.log(`${this.weight} pounds`);
     }
 
+    displayName(){
+        console.log(`your name is ${this.name}`);
+    }
+
+    displayAge(){
+        console.log(`your are ${this.age} old`);
+    }
+
+}
+
+class Programmer extends Person{
+    constructor(name,age,weight,language){
+        super(name,age,weight);
+        this.language = language;
+    }
+
+    displayLanguage(){
+        console.log(`Programming language: ${this.language}`);
+    }
 }
 
 let bucky = new Person('johnson', 23, 7000);
+bucky.displayName();
+bucky.displayAge();
 bucky.displayWeight();
+
+let johnson = new Programmer('Johnson O',23,7000,'JavaScript');
+johnson.displayName();
+johnson.displayAge();
+johnson.displayWeight();
+johnson.displayLanguage();
