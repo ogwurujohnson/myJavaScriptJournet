60450abf7d209d7d625221639ff51ee9951b48a0
blackhatjohnny <ogwurujohnson@gmail.com>
2018-04-03 12:29:02 +0100

    es6-template literals

---
+++ es6/templiteral.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let name = 'Bucky';
+
+console.log('My favorite person is '+ name +'because he smells nice');
+//an easier way to use variables in strings-using template litrals
+
+console.log(`My favorite person is ${name} becasue he smells good`);
+
+//you can do dis too;
+
+let a =5;
+let b = 7;
+
+console.log(`My favorite person is ${a+b} becasue he smells good`);
+
+//with template literal you dont need keywords to specify new line just press enter and put the next sentence like below
+console.log(`My favorite person is ${name} because  //pressed enter here
+he smells good`);
+
